f198f58ec5ed53dd42d9577f6cca97e2f1cbf6b5
Ethan Schneider <ethannkschneider@gmail.com>
2017-11-10 01:12:39 -0500

    Finish basic sequencing functionality

--- scripts/entry.js
+++ scripts/entry.js
@@ -5,19 +5,31 @@ let audioContext;
 let oscillator;
 let clock;
 let isPlaying = false;
-let sounds = ["hh1, kick1, snare1"];
+let sounds = ["hh1", "kick1", "snare1"];
 let buffers = {};
-
+let tempo = 120;
+let totalBeats = 4;
+let beatTime = 60 / tempo;
+let barTime = totalBeats * beatTime;
+let beats = {
+  "hh1": {1: null, 2: null, 3: null, 4: null},
+  "kick1": {1: null, 2: null, 3: null, 4: null},
+  "snare1": {1: null, 2: null, 3: null, 4: null}
+};
+let allEvents = [];
 
 document.addEventListener('DOMContentLoaded', () => {
-
   setupAudioContext();
   setupClock();
   setupClickHandlers();
+
   sounds.forEach( (sound) => {
     loadSound(sound);
   });
+  //START TEST//
+  window.beats = beats;
   window.buffers = buffers;
+  //END TEST//
 });
 
 const setupAudioContext = () => {
@@ -34,13 +46,19 @@ const setupAudioContext = () => {
 
 const setupClock = () => {
   // Initialize the WAAClock
-  clock = new WAAClock(audioContext);
+  clock = new WAAClock(audioContext, {toleranceEarly: 0.1});
 };
 
 const setupClickHandlers = () => {
   //Nodes on the sequencer change color when clicked
+  //They also either activate or deactive their respective events
   $(".grid-node").click( (e) => {
-    $(e.target).toggleClass("clicked-node");
+    let $node = $(e.target);
+    let nodeBeat = $node.data("beat");
+    let nodeSound = $node.data("sound");
+    $node.toggleClass("clicked-node");
+    $node.hasClass("clicked-node") ?
+      activateNode(nodeBeat, nodeSound) : deactivateNode(nodeBeat, nodeSound);
   });
 
   //Start with the pause icon hidden and then toggle on click
@@ -54,12 +72,54 @@ const setupClickHandlers = () => {
   });
 };
 
+// Schedule the node to play its sound on the given beat
+const activateNode = (beat, sound) => {
+  let event = clock.callbackAtTime( (newEvent) => {
+    let bufferNode = buffers[sound].createNode();
+    bufferNode.start(newEvent.deadline);
+  }, nextBeatTime(beat));
+  event.repeat(barTime);
+  event.tolerance({late: 0.01});
+  beats[sound][beat] = event;
+};
+
+const deactivateNode = (beat, sound) => {
+  let event = beats[sound][beat];
+  event.clear();
+  beats[sound][beat] = null;
+};
+
+const nextBeatTime = (beat) => {
+  let currentTime = audioContext.currentTime;
+  let currentBar = Math.floor(currentTime / barTime);
+  let currentBeat = Math.round(currentTime % barTime);
+  if (currentBeat < beat) {
+    return currentBar * barTime + beat * beatTime;
+  } else {
+    return (currentBar + 1) * barTime + beat * beatTime;
+  }
+};
+
 const handlePlay = () => {
+  clock.start();
+  //Starting the clock clears all prior events, so we reactivate here:
+  activateNodes();
 
 };
 
 const handlePause = () => {
+  clock.stop();
+};
 
+// Iterate through all clicked nodes, pull out their data-attributes,
+// and activate
+const activateNodes = () => {
+  const $clickedNodes = $(".clicked-node");
+  $clickedNodes.each ( function(index, el) {
+    let beat = $(this).data("beat");
+    let sound = $(this).data("sound");
+    activateNode(beat, sound);
+  });
 };
 
 const loadSound = (instrumentName) => {
@@ -74,6 +134,7 @@ const loadSound = (instrumentName) => {
   // We then store that particular createNode function in the buffers array
   // so we can access it later.
   oReq.onload = () => {
+    console.log("Request loaded!");
     audioContext.decodeAudioData(oReq.response, (audioBuffer) => {
       let createNode = function() {
         let source = audioContext.createBufferSource();
@@ -82,9 +143,17 @@ const loadSound = (instrumentName) => {
         return source;
       };
       buffers[instrumentName] = { createNode };
+      console.log("buffer added!");
     });
   };
 
   // Actually send the request
   oReq.send();
 };
+
+
+
+
+
+
+// End of document
